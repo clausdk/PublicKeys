135b7a67a13345ac7073a6d1830567e48ac031ed
clausdk <cba@1it.dk>
2021-02-06 23:51:40 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo Installing SSH key on the active account
+mkdir -p ~/.ssh/
 sudo wget https://raw.githubusercontent.com/clausdk/PublicKeys/master/authorized_keys -O ~/.ssh/authorized_keys
